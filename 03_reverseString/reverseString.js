dd565dbd36e1529da2fc730522fd494b183dcb7d
leeyihjie <leeyihjie@yahoo.com.sg>
2023-12-31 03:09:54 +0800

    Finished reversed string exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var revStr = '';
+    for (var i = str.length -1; i >= 0; i--) {
+        revStr += str[i];
+    }
+    return revStr;
 };
 
 // Do not edit below this line
