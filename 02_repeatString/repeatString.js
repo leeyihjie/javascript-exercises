bfb06a0ad810956179ddb11e40d6a93f4fed2e77
leeyihjie <leeyihjie@yahoo.com.sg>
2023-12-31 03:02:31 +0800

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    let concStr = '';
 
+    if (count < 0) {
+        concStr = 'ERROR';
+        return concStr
+    };
+    
+    for (i=0;i<count;i++) {
+        concStr = concStr + str;
+    };
+
+    return concStr;
 };
 
 // Do not edit below this line
