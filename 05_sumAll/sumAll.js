35a3c2abf42e5a0781ef2d5650eece337c1cad76
leeyihjie <leeyihjie@yahoo.com.sg>
2024-01-02 19:18:08 +0800

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    var small = 0;
+    var large = 0;
+    var sum = 0;
 
+    if (int1 < 0 | int2 <0 | typeof(int1) != 'number' | typeof(int2) != 'number')  {
+        return "ERROR"
+    }
+
+    if (int1 < int2) {
+        small = int1;
+        large = int2;
+    }
+    else if (int2 < int1) {
+        small = int2;
+        large = int1;
+    }
+    else {
+        small = int1;
+        large = int2;
+    }
+
+    console.log("Range of Numbers to be added: " + small + " - " + large);
+
+
+    for (var i = small; i <= large; i++) {
+        sum = sum + i;
+    }
+
+    console.log("Total Sum: " + sum);
+
+    return sum;
 };
 
 // Do not edit below this line
