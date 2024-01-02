ad6d38f551bfbca690e059365b9f54abbdc89a90
leeyihjie <leeyihjie@yahoo.com.sg>
2024-01-02 19:37:57 +0800

    Finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahTemp) {
+  var convCelTemp = ((fahTemp - 32) * (5 / 9)).toFixed(1);
+  console.log("Fah to Cel: " + parseFloat(convCelTemp))
+  return parseFloat(convCelTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  var convFahTemp = (celTemp * (9 / 5) + 32).toFixed(1);
+  console.log("Cel to Fah: " + parseFloat(convFahTemp))
+  return parseFloat(convFahTemp);
 };
 
 // Do not edit below this line
