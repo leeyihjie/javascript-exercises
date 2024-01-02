03bfe9b1bd217999af3277dc2e9aaaa522638015
leeyihjie <leeyihjie@yahoo.com.sg>
2024-01-02 19:07:20 +0800

    exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...selection) {
+    console.log("Original Array: " + arr);
+
+    console.log("Numbers to be removed: " + selection);
+    for (var i = 0; i < selection.length; i++) {
+        var index = arr.indexOf(selection[i]);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    console.log("New Array: " + arr);
+    return arr;
 
 };
 
