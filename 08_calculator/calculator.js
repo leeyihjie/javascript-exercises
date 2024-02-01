62d1c3ff598d810d8bb2ae68234db842dafe17fb
leeyihjie <leeyihjie@yahoo.com.sg>
2024-02-01 23:11:16 +0800

    Finished 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  addedNum = firstNum + secondNum;
+
+  return addedNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+  subtractedNum = firstNum - secondNum;
+
+  return subtractedNum;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	summedNumArray = numArray.reduce(function(total, num) {
+    summed = total + num;
+    return summed;
+  }, 0);
+
+  return summedNumArray;
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  if(numArray.length == 0) {
+    return 0;
+  }
 
+  multipliedNumArray = numArray.reduce(function(total, num) {
+    multiplied = total * num;
+    return multiplied;
+  }, 1);
+
+  return multipliedNumArray;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return Math.pow(num, power)
 };
 
-const factorial = function() {
-	
+const factorial = function(facNum) {
+  if (facNum === 0) {
+    return 1
+  };
+
+  let product = 1;
+  for (let i = 1; i <= facNum; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
