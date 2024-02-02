99fda79575480c261bf7cc9176a6d6371707e077
leeyihjie <leeyihjie@yahoo.com.sg>
2024-02-02 22:23:22 +0800

    12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldest = arr.sort(function(a,b) {
+        if (a.yearOfDeath == undefined) {
+            a.yearOfDeath = 2024;
+        }
+        else if (b.yearOfDeath == undefined) {
+            b.yearOfDeath = 2024;
+        }
 
+        firstPersonAge = a.yearOfDeath - a.yearOfBirth;
+        console.log("a death year: ", a.yearOfDeath)
+        console.log("a age: ", firstPersonAge)
+
+
+
+        nextPersonAge = b.yearOfDeath - b.yearOfBirth;
+        console.log("b death year: ", b.yearOfDeath)
+        console.log("b age: ", nextPersonAge)
+
+
+        if (firstPersonAge > nextPersonAge) {
+            console.log("-1")
+            return -1;
+        }
+        console.log("1")
+
+        return 1;
+
+    });
+
+    return (oldest[0])
 };
 
 // Do not edit below this line
