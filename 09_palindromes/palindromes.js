a72f5e94fd86c1feb2cbf2c63dafb785dd467d97
leeyihjie <leeyihjie@yahoo.com.sg>
2024-02-02 15:29:57 +0800

    09 exercise Palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strReplaced = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    console.log(strReplaced);
 
+    const strReplacedArray = strReplaced.split("");
+    console.log(strReplacedArray);
+
+    const strReplacedArrayReversed = strReplacedArray.reverse();
+    console.log(strReplacedArrayReversed);
+
+    const reversedStr = strReplacedArrayReversed.join("");
+    console.log(reversedStr);
+
+    if (reversedStr === strReplaced) {
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
