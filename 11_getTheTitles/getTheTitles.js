bd01d4105cb2f116ceb8e43b9d316a616c99751d
leeyihjie <leeyihjie@yahoo.com.sg>
2024-02-02 16:19:22 +0800

    11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArr) {
+    let result = bookArr.map(item => item.title);
+
+    console.log(result);
+
+    return result;
 
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
