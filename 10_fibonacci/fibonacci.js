c5a914ec9a76060c3aa5b536f5610d1f3410ab64
leeyihjie <leeyihjie@yahoo.com.sg>
2024-02-02 15:57:12 +0800

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    };
+
+    console.log("Type of index before conversion: " + typeof index);
+
+    intIndex = parseInt(index);
+    console.log("Type of index after conversion: " + typeof intIndex);
+
+    const startArr = [0,1];
+
+    for (let i = 2; i <= index; i++) {
+        startArr.push(startArr[i-2]+startArr[i-1]);
+        console.log(startArr);
+    }
+
+    return startArr[index];
 
 };
 
